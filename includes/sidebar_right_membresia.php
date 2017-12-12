bf79746629ae876a04c5c32ce9f649d64ab45925
neobenjax <besaze@hotmail.com>
2017-12-11 18:05:30 -0600

    Actualizacion de membresias

--- includes/sidebar_right_membresia.php
+++ includes/sidebar_right_membresia.php
@@ -22,9 +22,12 @@
       <li>Opci&oacute;n de Pagos a Meses Sin Intereses con tarjetas bancarias participantes.</li>
     </ul>
     <p></p>
+    <h4 style="font-size:19px; text-align:left;">Y en exclusivo por el mes de Diciembre de 2017, 3 a&ntilde;os de Limpiezas Dentales por un pago &uacute;nico de tan solo $400*</h4>
+    <p></p>
     <h4 style="font-size:19px; text-align:left;">Para obtener nuestra membres&iacute;a, acude a cualquiera de nuestras sucursales</h4>
     <p>  <font color="white"> . </font></p>
-    <h4 style="font-size:19px; text-align:left;">O si lo prefieres, puedes adquirirla en l&iacute;nea y obtendr&aacute;s el 50% de descuento</h4>
+    <!--COMENTARIO <h4 style="font-size:19px; text-align:left;">O si lo prefieres, puedes adquirirla en l&iacute;nea y obtendr&aacute;s el 50% de descuento</h4>  -->
+    <h4 style="font-size:19px; text-align:left;">O si lo prefieres, puedes adquirirla en l&iacute;nea</h4>
     <p>  <font color="white"> . </font></p>
     <p>Solo haz clic en  PAGO EN L&Iacute;NEA y disfruta de este beneficio con total seguridad:</p>
     <p></p>
@@ -41,6 +44,8 @@
     <p> </p>
     <li>*Al adquirir la Membres&iacute;a Sonr&iacute;a adquieres los beneficios de ser Paciente Sonr&iacute;a. El costo de la Membres&iacute;a Sonr&iacute;a no es reembolsable.</li>
     <li>*Descuento no acumulable con otras promociones, descuentos, ofertas, precios especiales, o convenios. En caso de tomar la Opci&oacute;n de pagos a meses sin intereses, no aplican descuestos o promociones adicionales.</li>
+    <li>*En un periodo de hasta 3 a&ntilde;os a partir de la compra de la membresia, se realizaran hasta 6 eliminaciones de sarro supragingival en boca completa, la cual consiste en eliminacion de sarro mediante tecnica tradicional o mediante procedimiento con ultrasonido .</li>
+    <li>*Se debe llamar a la sucursal para poder agendar una cita de Domingo a Viernes sujeto a disponibilidad y horarios de cada sucursal.</li>
       <!--COMENTARIO /*<p><p>Eres usuario <a href="?s=membresiapaypal"><img src="img/logopaypal.png" /></a>  ?</p></p> */ -->
   </div>
 </div>
